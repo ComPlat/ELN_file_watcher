d7def97bfe65f66fd422189771b94dde515efae2
Martin Starman <martin.starman@kit.edu>
2022-07-27 14:49:53 +0200

    Update run scripts

--- bin/run386_example.bat
+++ bin/run386_example.bat
@@ -1,1 +1,1 @@
-efw_win386.exe -duration <integer> -src <folder> -dst https://os-webdav.lsdf.kit.edu/<OE>/<inst>/projects/<PROJECTNAME>/ -user <username> -pass <password> [-zip]
+efw_win386.exe -duration <time_in_sec> -src "<path_to_folder>" -dst "https://os-webdav.lsdf.kit.edu/<OE>/<inst>/projects/<PROJECTNAME>/" -user "<username>" -pass "<password>" [-crt "<path_to_server_crt>"] [-zip]
---
+++ bin/run64_example.bat
@@ -0,0 +1,1 @@
+efw_win64.exe -duration <time_in_sec> -src "<path_to_folder>" -dst "https://os-webdav.lsdf.kit.edu/<OE>/<inst>/projects/<PROJECTNAME>/" -user "<username>" -pass "<password>" [-crt "<path_to_server_crt>"] [-zip]
